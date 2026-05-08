fdbd897cd774aa6747621023058acaa305c6d4ee
syaofox <syaofox@gmail.com>
2026-05-08 22:09:22 +0800

    移除无用的脚本

--- setup/install-slstatus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
-
-log_step "Compile and install slstatus..."
-
-compile_and_install "slstatus" "https://github.com/syaofox/slstatus.git" "/tmp/slstatus"
-exit $?
