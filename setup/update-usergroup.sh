01865fa8c102146630ee0b2d3eab644dc8055457
syaofox <syaofox@gmail.com>
2026-05-09 12:20:44 +0800

    123123

--- setup/update-usergroup.sh
+++ setup/update-usergroup.sh
@@ -4,7 +4,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
 log_step "Updating user groups..."
 
-sudo usermod -aG video,render $USER || exit 1
+sudo usermod -aG video,render,input $USER || exit 1
 
 log_step "Creating user directories..."
 
