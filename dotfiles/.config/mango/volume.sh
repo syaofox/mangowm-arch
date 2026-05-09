a70d63d4fc9b7ddbf9dc41e9b241246dfb0cf312
syaofox <syaofox@gmail.com>
2026-05-09 12:23:47 +0800

    123123

--- dotfiles/.config/mango/volume.sh
+++ dotfiles/.config/mango/volume.sh
@@ -10,20 +10,20 @@ case "$mode" in
     up)
         pactl set-sink-volume @DEFAULT_SINK@ +5%
         vol=$(get_volume)
-        notify-send -r 9988 -t 500 "Volume ${vol}%"
+        notify-send -r 9988 -t 500 -h int:value:${vol} -h string:x-canonical-private-synchronous:volume "Volume"
         ;;
     down)
         pactl set-sink-volume @DEFAULT_SINK@ -5%
         vol=$(get_volume)
-        notify-send -r 9988 -t 500 "Volume ${vol}%"
+        notify-send -r 9988 -t 500 -h int:value:${vol} -h string:x-canonical-private-synchronous:volume "Volume"
         ;;
     mute)
         pactl set-sink-mute @DEFAULT_SINK@ toggle
         if pactl get-sink-mute @DEFAULT_SINK@ | grep -q yes; then
-            notify-send -r 9988 -t 1000 "Muted"
+            notify-send -r 9988 -t 1000 -h string:x-canonical-private-synchronous:volume "Muted"
         else
             vol=$(get_volume)
-            notify-send -r 9988 -t 500 "Volume ${vol}%"
+            notify-send -r 9988 -t 500 -h int:value:${vol} -h string:x-canonical-private-synchronous:volume "Volume"
         fi
         ;;
 esac
